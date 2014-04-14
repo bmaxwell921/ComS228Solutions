ebdca410330aab0b025cc6ebbfad3a9dab3e32fd
Brandon <bmaxwell921@gmail.com>
2014-04-13 22:54:21 -0500

    Default implementation of Node

--- src/edu/iastate/cs228/hw5/Node.java
+++ src/edu/iastate/cs228/hw5/Node.java
@@ -6,80 +6,66 @@ package edu.iastate.cs228.hw5;
  *
  */
 
-
 /**
  * 
- * This class represents a tree node.  The class is public but in the BST class 
- * the root node will be protected. 
- *
+ * This class represents a tree node. The class is public but in the BST class
+ * the root node will be protected.
+ * 
  */
 
-// also okay to use the following. 
+// also okay to use the following.
 // public class Node<E extends Comparable<? super E>>
-public class Node<E> 
-{
-	private E data; 
-	private Node<E> parent; 
-	private Node<E> left; 
-	private Node<E> right; 
-	
-	
-	public Node(E dat)
-	{
+public class Node<E> {
+	private E data;
+	private Node<E> parent;
+	private Node<E> left;
+	private Node<E> right;
+
+	public Node(E dat) {
 		// TODO
 	}
 
-	public Node(E dat, Node<E> left, Node<E> right)
-	{
+	public Node(E dat, Node<E> left, Node<E> right) {
 		// TODO
 	}
-	
+
 	/**
 	 * Write the value of the instance variable named data.
 	 */
-	public String toString()
-	{
-		// TODO
+	public String toString() {
+		return "Node: " + data;
 	}
 
-	public E getData()
-	{
-		// TODO
+	public E getData() {
+		return data;
 	}
-	
-	public void setData(E dat)
-	{
-		// TODO
+
+	public void setData(E dat) {
+		this.data = dat;
 	}
-	
-	public Node<E> getParent()
-	{
-		// TODO
+
+	public Node<E> getParent() {
+		return parent;
 	}
-	
-	public void setParent(Node<E> parent)
-	{
-		// TODO 
+
+	public void setParent(Node<E> parent) {
+		this.parent = parent;
 	}
-	
-	public Node<E> getLeft()
-	{
-		// TODO
+
+	public Node<E> getLeft() {
+		return left;
 	}
-	
-	public void setLeft(Node<E> left)
-	{
-		// TODO
+
+	public void setLeft(Node<E> left) {
+		this.left = left;
 	}
-	
-	public Node<E> getRight()
-	{
-		// TODO
+
+	public Node<E> getRight() {
+		return right;
 	}
-	
-	public void setRight(Node<E> right)
-	{
-		// TODO
+
+	public void setRight(Node<E> right) {
+		this.right = right;
 	}
-	
+
 }
