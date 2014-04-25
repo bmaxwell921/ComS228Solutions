570baba93067f8928542c668420edfb09e0e26d3
Brandon <bmaxwell921@gmail.com>
2014-04-25 13:22:42 -0500

    Formatting and adding serialization version

--- src/edu/iastate/cs228/hw5/TreeStructureException.java
+++ src/edu/iastate/cs228/hw5/TreeStructureException.java
@@ -9,16 +9,17 @@ package edu.iastate.cs228.hw5;
 /**
  * Expression thrown in PostfixExample for an invalid expression.
  */
-public class TreeStructureException extends Exception
-{
-  public TreeStructureException()
-  {
-    super();
-  }
-  
-  public TreeStructureException(String msg)
-  {
-    super(msg);
-  }
-}
+public class TreeStructureException extends Exception {
+	/**
+	 * Initial version
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public TreeStructureException() {
+		super();
+	}
+
+	public TreeStructureException(String msg) {
+		super(msg);
+	}
+}
