65e29d11ddcb61f9a6180fd0f35f537b60fe684d
Brandon <bmaxwell921@gmail.com>
2014-04-25 13:22:32 -0500

    Formatting

--- src/edu/iastate/cs228/hw5/TreeSort.java
+++ src/edu/iastate/cs228/hw5/TreeSort.java
@@ -1,21 +1,20 @@
 package edu.iastate.cs228.hw5;
 
 /**
- *  
+ * 
  * @author
  *
  */
 
-
-public class TreeSort
-{
+public class TreeSort {
 	/**
-	 * Sorts an input array by first creating a binary search tree to store its elements, and then perform an 
-	 * inorder traversal of the tree to visit the elements in order and put them back in the array. 
+	 * Sorts an input array by first creating a binary search tree to store its
+	 * elements, and then perform an inorder traversal of the tree to visit the
+	 * elements in order and put them back in the array.
+	 * 
 	 * @param eleArray
 	 */
-	public static <E extends Comparable<? super E>> void sort(E[] eleArray)
-	{
-		// TODO 
+	public static <E extends Comparable<? super E>> void sort(E[] eleArray) {
+		// TODO
 	}
 }
