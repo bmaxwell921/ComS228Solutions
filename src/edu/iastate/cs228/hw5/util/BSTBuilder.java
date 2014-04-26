aeae6996f357e374fa6e00af1fcf39f6567b2f61
Brandon <bmaxwell921@gmail.com>
2014-04-25 20:08:55 -0500

    Reworked to use Lists and made it so students don't have to provide the full tree

--- src/edu/iastate/cs228/hw5/util/BSTBuilder.java
+++ src/edu/iastate/cs228/hw5/util/BSTBuilder.java
@@ -1,6 +1,10 @@
 package edu.iastate.cs228.hw5.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import edu.iastate.cs228.hw5.BST;
 import edu.iastate.cs228.hw5.Node;
@@ -9,26 +13,23 @@ import edu.iastate.cs228.hw5.Node;
  * Utility class used to build BSTs
  * 
  * @author Brandon
- *
+ * 
  */
 public class BSTBuilder {
 
 	/**
-	 * Builds a BST with the given information. Levels is a 2d list,
-	 * levels.get(0) should be a list of all the nodes at level 0 (i.e. just the
-	 * root), levels.get(1) should have exactly 2 elements in it. If one element
-	 * of levels.get(i) is null, that means it is an empty spot. The tree will
-	 * be filled in from left to right, for all non null elements.
-	 * 
-	 * Note: This method will make sure you're only building valid BSTs
-	 * 
-	 * Examples: input: {{B}, {A, null}} output: B / A
-	 * 
-	 * input: {{C}, {null, E}, {D, F}} output: C \ E / \ E F Example usage:
+	 * Builds a BST object with the given information. See the included README
+	 * for detailed use.
 	 * 
 	 * @param levels
-	 * @return The BST represented by the given List, or an empty tree is levels
+	 * @return The BST represented by the given List, or an empty tree if levels
 	 *         is null or empty
+	 * @throws IllegalArgumentException
+	 *             If not enough information is provided
+	 * @throws IllegalArgumentException
+	 *             If you attempt to build a BST that violates BST ordering
+	 * @throws IllegalArgumentException
+	 *             If you have duplicate values in the given array
 	 */
 	public static <E extends Comparable<? super E>> BST<E> buildBST(E[][] levels) {
 		// Empty tree
@@ -36,23 +37,51 @@ public class BSTBuilder {
 			return new BST<E>(null, 0);
 		}
 
-		Node<E> root = buildRoot(levels[0]);
-		buildTree((Node<E>[]) new Object[] { root }, 1, levels);
+		if (hasDuplicates(levels)) {
+			duplicateValues();
+		}
+
+		final Node<E> root = buildRoot(levels[0]);
+		List<Node<E>> l = new ArrayList<>();
+		l.add(root);
+
+		buildTree(l, 1, levels);
 
 		int size = countSize(levels);
 
 		return new BST<>(root, size);
 	}
 
+	// Checks for dupliate values in the tree
+	private static boolean hasDuplicates(Object[][] levels) {
+		final Set<Object> found = new HashSet<>();
+		for (Object[] level : levels) {
+			if (level == null) {
+				continue;
+			}
+			for (Object o : level) {
+				if (o == null)
+					continue;
+				if (found.contains(o)) {
+					return true;
+				}
+				found.add(o);
+			}
+		}
+		return false;
+	}
+
 	/*
 	 * Creates the root node
 	 */
-	private static <E extends Comparable<? super E>> Node<E> buildRoot(
-			E[] level0) {
+	private static <E extends Comparable<? super E>> Node<E> buildRoot(E[] level0) {
 		if (level0 == null || !hasProperLevelCount(0, level0))
-			throw new IllegalArgumentException(
-					"The input list has the wrong amount of elements for level 0.");
+			throw new IllegalArgumentException("The input list has the wrong amount of elements for level 0.");
 		return new Node<E>(level0[0]);
+	}
+
+	private static boolean hasProperChildCount(int numParents, Object[] children) {
+		return children != null && children.length == (numParents * 2);
 	}
 
 	/*
@@ -71,6 +100,13 @@ public class BSTBuilder {
 		return (int) Math.pow(2, level);
 	}
 
+	/*
+	 * Each parent should have all their children defined, even if they're null
+	 */
+	private static int getProperChildCount(int parentCount) {
+		return parentCount * 2;
+	}
+
 	/**
 	 * Builds the tree recursively, with the given nodes
 	 * 
@@ -81,21 +117,19 @@ public class BSTBuilder {
 	 * @param levels
 	 *            All the levels to build from
 	 */
-	private static <E extends Comparable<? super E>> void buildTree(
-			Node<E>[] prevNodes, int curLevel, E[][] levels) {
+	private static <E extends Comparable<? super E>> void buildTree(List<Node<E>> prevNodes, int curLevel, E[][] levels) {
 		// Base case
 		if (curLevel == levels.length) {
 			return;
 		}
 
-		// Error case:
-		if (!hasProperLevelCount(curLevel, levels[curLevel])) {
-			// Throw exception
-			invalidLevelCount(curLevel, levels[curLevel]);
+		// Error case
+		if (!hasProperChildCount(prevNodes.size(), levels[curLevel])) {
+			invalidChildCount(prevNodes, levels[curLevel]);
 		}
 
 		// If it's no an error, move on
-		Node<E>[] curNodes = buildCurrentLevel(prevNodes, curLevel, levels);
+		List<Node<E>> curNodes = buildCurrentLevel(prevNodes, curLevel, levels);
 
 		// Recurse
 		buildTree(curNodes, ++curLevel, levels);
@@ -105,54 +139,59 @@ public class BSTBuilder {
 	 * Builds all the nodes for the current level. Returns an array of the nodes
 	 * that were created.
 	 */
-	private static <E extends Comparable<? super E>> Node<E>[] buildCurrentLevel(
-			Node<E>[] prevNodes, int curLevel, E[][] levels) {
-		Node<E>[] curNodes = (Node<E>[]) new Object[getProperLevelCount(curLevel)];
+	private static <E extends Comparable<? super E>> List<Node<E>> buildCurrentLevel(List<Node<E>> prevNodes, int curLevel, E[][] levels) {
+		List<Node<E>> newNodes = new ArrayList<>();
 		// Loop thru all the previous nodes
+		int childNum = 0;
 		for (Node<E> prevNode : prevNodes) {
-			// And go thru all the data. We'll manually update i in the body
-			for (int i = 0; i < levels[curLevel].length;) {
-				// If the previous node was null, add two
-				// null children for it, but don't link them in
-				if (prevNode == null) {
-					curNodes[i++] = null;
-					curNodes[i++] = null;
-					continue;
-				}
+			// Grab the data, post increment to update childNum too 
+			E left = levels[curLevel][childNum++];
+			E right = levels[curLevel][childNum++];
 
-				// Otherwise we need to link in the children
-				E left = levels[curLevel][i];
-				E right = levels[curLevel][i + 1];
+			// Check that it's not an invalid BST
+			if (!(left == null || left != null && validLeft(left, prevNode.getData()))) {
+				invalidBST(prevNode.getData(), left, right);
+			}
+			if (!(right == null || right != null && validRight(right, prevNode.getData()))) {
+				invalidBST(prevNode.getData(), left, right);
+			}
 
-				if (!validBST(left, right, prevNode.getData())) {
-					// Throw exception
-					invalidBST(prevNode.getData(), left, right);
-				}
+			// Add the new nodes
+			Node<E> lNode = left != null ? new Node<E>(left) : null;
+			Node<E> rNode = right != null ? new Node<E>(right) : null;
+			linkInNodes(prevNode, lNode, rNode);
 
-				// Add the new nodes
-				Node<E> lNode = new Node<E>(left);
-				Node<E> rNode = new Node<E>(right);
-				linkInNodes(prevNode, lNode, rNode);
-				curNodes[i++] = lNode;
-				curNodes[i++] = rNode;
+			// We only add non-null stuff so if one of prevNode's children are null, we just skip them later down the tree
+			if (lNode != null) {
+				newNodes.add(lNode);
+			}
+			if (rNode != null) {
+				newNodes.add(rNode);
 			}
 		}
-		return curNodes;
+		return newNodes;
 	}
 
 	// Checks to make sure that left is < parent and right is > parent
-	private static <E extends Comparable<? super E>> boolean validBST(E left,
-			E right, E parent) {
-		return left.compareTo(parent) < 0 && right.compareTo(parent) > 0;
+	private static <E extends Comparable<? super E>> boolean validLeft(E left, E parent) {
+		return left.compareTo(parent) < 0;
+	}
+
+	// Checks to make sure that left is < parent and right is > parent
+	private static <E extends Comparable<? super E>> boolean validRight(E right, E parent) {
+		return right.compareTo(parent) > 0;
 	}
 
 	// Sets up the links between parent and children
-	private static <E extends Comparable<? super E>> void linkInNodes(
-			Node<E> parent, Node<E> left, Node<E> right) {
+	private static <E extends Comparable<? super E>> void linkInNodes(Node<E> parent, Node<E> left, Node<E> right) {
 		parent.setLeft(left);
 		parent.setRight(right);
-		left.setParent(parent);
-		right.setParent(parent);
+		if (left != null) {
+			left.setParent(parent);
+		}
+		if (right != null) {
+			right.setParent(parent);
+		}
 	}
 
 	/*
@@ -161,6 +200,9 @@ public class BSTBuilder {
 	private static int countSize(Object[][] levels) {
 		int size = 0;
 		for (Object[] level : levels) {
+			if (level == null) {
+				continue;
+			}
 			for (Object obj : level) {
 				if (obj != null) {
 					++size;
@@ -175,15 +217,18 @@ public class BSTBuilder {
 	 * Exceptions
 	 * ----------------------------------------------------------------
 	 */
-	private static void invalidLevelCount(int level, Object[] data) {
+	private static void invalidChildCount(List<?> parents, Object[] children) {
 		throw new IllegalArgumentException(String.format(
-				"Invalid level count for level: %d. Values: %s", level,
-				Arrays.toString(data)));
+				"Invalid number of children for the number of parents. Was: %d, Expected: %d. Parents: %s, Children: %s", children.length,
+				getProperChildCount(parents.size()), parents, Arrays.toString(children)));
 	}
 
 	private static void invalidBST(Object parent, Object left, Object right) {
-		throw new IllegalArgumentException(String.format(
-				"Tried to build invalid BST! Parent: %s, Left: %s, Right: %s",
-				parent, left, right));
+		throw new IllegalArgumentException(
+				String.format("Tried to build invalid BST! Parent: %s, Left: %s, Right: %s", parent, left, right));
+	}
+
+	private static void duplicateValues() {
+		throw new IllegalArgumentException("Tried to make a BST with duplicate values");
 	}
 }
