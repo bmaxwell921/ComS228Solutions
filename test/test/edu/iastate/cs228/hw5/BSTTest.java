dce0386f37403cd64c1686259f119993502ba541
Brandon <bmaxwell921@gmail.com>
2014-04-27 21:19:25 -0500

    Tests for copy constructor

--- test/test/edu/iastate/cs228/hw5/BSTTest.java
+++ test/test/edu/iastate/cs228/hw5/BSTTest.java
@@ -7,7 +7,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import edu.iastate.cs228.hw5.BST;
+import edu.iastate.cs228.hw5.Node;
+import edu.iastate.cs228.hw5.TreeStructureException;
 import edu.iastate.cs228.hw5.util.BSTBuilder;
+import edu.iastate.cs228.hw5.util.BSTValidator;
 
 /**
  * Class used to test my implementation of the BST class
@@ -25,6 +28,7 @@ public class BSTTest {
 
 	@Test
 	public void testHeight_Empty() {
+		// DON'T PUT THIS IN THE REAL TESTS!
 		tree = buildEmpty();
 		Assert.assertEquals(-1, tree.height());
 	}
@@ -166,7 +170,7 @@ public class BSTTest {
 		tree.getInorderSequence(seq);
 		Assert.assertEquals(correct, seq);
 	}
-	
+
 	@Test
 	public void testPostorderSeq_Empty() {
 		ArrayList<String> seq = new ArrayList<>();
@@ -202,97 +206,155 @@ public class BSTTest {
 		tree.getPostorderSequence(seq);
 		Assert.assertEquals(correct, seq);
 	}
-	
+
 	@Test
 	public void testTraversePre_Empty() {
 		String correct = "";
 		tree = buildEmpty();
 		Assert.assertEquals(correct, tree.traversePreorder().trim());
 	}
-	
+
 	@Test
 	public void testTraversePre_Small() {
 		String correct = "B A C";
 		tree = buildSmallTree();
 		Assert.assertEquals(correct, tree.traversePreorder().trim());
 	}
-	
+
 	@Test
 	public void testTraversePre_Medium() {
 		String correct = "D B A C F E G";
 		tree = buildMediumTree();
 		Assert.assertEquals(correct, tree.traversePreorder().trim());
 	}
-	
+
 	@Test
 	public void testTraversePre_Large() {
 		String correct = "H D B A C F E G L J I K N M O";
 		tree = buildLargeTree();
 		Assert.assertEquals(correct, tree.traversePreorder().trim());
 	}
-	
+
 	@Test
 	public void testTraverseIn_Empty() {
 		String correct = "";
 		tree = buildEmpty();
 		Assert.assertEquals(correct, tree.traverseInorder().trim());
 	}
-	
+
 	@Test
 	public void testTraverseIn_Small() {
 		String correct = "A B C";
 		tree = buildSmallTree();
 		Assert.assertEquals(correct, tree.traverseInorder().trim());
 	}
-	
+
 	@Test
 	public void testTraverseIn_Medium() {
 		String correct = "A B C D E F G";
 		tree = buildMediumTree();
 		Assert.assertEquals(correct, tree.traverseInorder().trim());
 	}
-	
+
 	@Test
 	public void testTraverseIn_Large() {
 		String correct = "A B C D E F G H I J K L M N O";
 		tree = buildLargeTree();
 		Assert.assertEquals(correct, tree.traverseInorder().trim());
 	}
-	
+
 	@Test
 	public void testTraversePost_Empty() {
 		String correct = "";
 		tree = buildEmpty();
 		Assert.assertEquals(correct, tree.traversePostorder().trim());
 	}
-	
+
 	@Test
 	public void testTraversePost_Small() {
 		String correct = "A C B";
 		tree = buildSmallTree();
 		Assert.assertEquals(correct, tree.traversePostorder().trim());
 	}
-	
+
 	@Test
 	public void testTraversePost_Medium() {
 		String correct = "A C B E G F D";
 		tree = buildMediumTree();
 		Assert.assertEquals(correct, tree.traversePostorder().trim());
 	}
-	
+
 	@Test
 	public void testTraversePost_Large() {
 		String correct = "A C B E G F D I K J M O N L H";
 		tree = buildLargeTree();
 		Assert.assertEquals(correct, tree.traversePostorder().trim());
 	}
-	
+
 	@Test
 	public void testToString_Medium() {
 		// Copied from BSTSet...why bother testing it much
 		String regex = "+D+B-A-C+F-E-G";
 		tree = buildMediumTree();
 		Assert.assertTrue(tree.toString().replaceAll("\\s*", "").equals(regex));
+	}
+
+	@Test
+	public void testCopyCons_Empty() throws TreeStructureException {
+		Node<String> oRoot = null;
+		tree = new BST<>(oRoot);
+		testTree(new String[][] {});
+	}
+
+	@Test
+	public void testCopyCons_Small() throws TreeStructureException {
+		Node<String> oRoot = buildSmallTree().getRoot();
+		tree = new BST<>(oRoot);
+		testTree(new String[][] { { "B" }, { "A", "C" } });
+	}
+
+	@Test
+	public void testCopyCons_Medium() throws TreeStructureException {
+		Node<String> oRoot = buildMediumTree().getRoot();
+		tree = new BST<>(oRoot);
+		testTree(new String[][] { { "D" }, { "B", "F" }, { "A", "C", "E", "G" } });
+	}
+
+	@Test
+	public void testCopyCons_Large() throws TreeStructureException {
+		Node<String> oRoot = buildLargeTree().getRoot();
+		tree = new BST<>(oRoot);
+		testTree(new String[][] { { "H" }, { "D", "L" }, { "B", "F", "J", "N" }, { "A", "C", "E", "G", "I", "K", "M", "O" } });
+	}
+
+	@Test
+	public void testCopyConsHoles_Medium() throws TreeStructureException {
+		String[][] input = new String[][] {{"D"}, {null, "F"}, {null, "G"}};
+		Node<String> oRoot = BSTBuilder.buildBST(input).getRoot();
+		tree = new BST<>(oRoot);
+		testTree(input);
+	}
+
+	@Test
+	public void testCopyConsHoles_Large() throws TreeStructureException {
+		String[][] input = new String[][] { { "H" }, { null, "L" }, { "J", null }, {"I", null} };
+		Node<String> oRoot = BSTBuilder.buildBST(input).getRoot();
+		tree = new BST<>(oRoot);
+		testTree(input);
+	}
+	
+	@Test (expected = TreeStructureException.class)
+	public void testCopyConsNotBST_Small() throws TreeStructureException {
+		Node<String> oRoot = new Node<>("A");
+		Node<String> left = new Node<>("B");
+		Node<String> right = new Node<>("C");
+		
+		oRoot.setLeft(left);
+		oRoot.setRight(right);
+		left.setParent(oRoot);
+		right.setParent(oRoot);
+		
+		tree = new BST<>(oRoot);
 	}
 
 	private static BST<String> buildEmpty() {
@@ -310,5 +372,17 @@ public class BSTTest {
 	private static BST<String> buildLargeTree() {
 		return BSTBuilder.buildBST(new String[][] { { "H" }, { "D", "L" }, { "B", "F", "J", "N" },
 				{ "A", "C", "E", "G", "I", "K", "M", "O" } });
+	}
+
+	private <E extends Comparable<? super E>> void testTree(E[][] data) {
+		/*
+		 * I'm not going to provide the BSTValidator class since it would give
+		 * away homework answers, but you should be able to figure out what each
+		 * method does by its name.
+		 */
+		Assert.assertFalse("BST should be created without cycles", BSTValidator.hasCycle(tree));
+		Assert.assertTrue("BST should be created with the proper references", BSTValidator.hasProperRefs(tree));
+		Assert.assertTrue("BST should be created with the proper size", BSTValidator.hasProperSize(tree, data));
+		Assert.assertTrue("BST should be created with all the data in the right place", BSTValidator.hasAllData(tree, data));
 	}
 }
